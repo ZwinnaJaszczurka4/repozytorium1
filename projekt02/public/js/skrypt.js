eeb78fe3316d2eb543c4f8f9543b5aafa01ce2f7
oliwia <oliwiabujko1@gmail.com>
2025-11-11 14:30:51 +0000

    ZMIANY

--- projekt02/public/js/skrypt.js
+++ projekt02/public/js/skrypt.js
@@ -1,55 +1,49 @@
+let odsloniete = ""; 
+let licznik = 0;     
+
+document.addEventListener("DOMContentLoaded", () => {
+        start();
+});
+function start() {
+    const hasloElement = document.getElementById("slowo");
+    const haslo = hasloElement.dataset.haslo;
+
+    odsloniete = "x".repeat(haslo.length);
+    licznik = 0;
+
+    document.getElementById("kreski").textContent = odsloniete;
+    document.getElementById("wynik").textContent = "";
+}
 
 function sprawdz(){
     const hasloElement = document.getElementById("slowo"); 
     const litera = document.getElementById("litera").value; 
-
     const haslo = hasloElement.dataset.haslo;
-    let trwanie = true;
-    let licznik = 0;
+
     let trafiona = false;
 
-    let string = "";
+    let myArray = odsloniete.split("");
+
+    for (let i = 0; i < haslo.length; i++) {
+        if (litera == haslo[i] && myArray[i] == "x") {
+            myArray[i] = litera;
+            licznik++;
+            trafiona = true;
+        }
+    }
+
+    odsloniete = myArray.join("");
+    document.getElementById("kreski").textContent = odsloniete;
 
     let wynik = document.getElementById("wynik");
 
-    dlslowa = haslo.length;
-
-    for (let i = 0; i < dlslowa; i++){
-        string = string + "x";
-    }
-
-    let kreski = document.getElementById("kreski");
-    kreski.textContent = string;
-
-    console.log(string);
-    console.log("haslo ", haslo);
-
-    const myArray = string.split("");
-
-    //while(trwanie){
-        for (let i = 0; i < dlslowa; i++) {
-            if (litera==haslo[i]){
-                licznik++;
-                myArray[i] = litera;
-                trafiona = true;
-            };
-    };
-
-    string = myArray.join("");
-
-
-    if (trafiona){
-        kreski.textContent = string;
-        wynik.textContent = "Gratulacje! Literka " + litera + " znajduje sie w hasle!"; 
-    }else{
+    if (trafiona) {
+        wynik.textContent = "Gratulacje! Literka " + litera + " znajduje się w hasle!";
+    } else {
         wynik.textContent = "Niestety, nie ma litery " + litera + " w hasle!";
     }
 
-    if (licznik == dlslowa){
-        
+    if (licznik == haslo.length) {
+        wynik.textContent = "Koniec gry! Wygrales!";
     }
-
-
-    //};
-
-};
+}
